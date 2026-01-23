606aba368d6bad0f7e5b55659b780afd6d54c3ee
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-01-23 23:41:02 +0530

    Started Assingment 2.. solved some basic loops questions

---
+++ Assingment2/PrintNto1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n ;
+    cin>>n;
+    for(int i = n; i>=1 ; i--){
+         cout<< i  ;
+        if(i>1){
+            cout<< " ";
+        }
+       
+
+    }
+    return 0;
+}
