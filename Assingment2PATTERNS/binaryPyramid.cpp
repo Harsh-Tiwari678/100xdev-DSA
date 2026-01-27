ac8fbc4a809cf3e697cfd69ffc6b726cca16eff5
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-01-27 23:48:20 +0530

    solved some new patterns

---
+++ Assingment2PATTERNS/binaryPyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    for(int i = 1; i<=n ; i++){
+        int start;
+        if(i%2!=0){
+            start = 0;
+        }
+        else{
+            start = 1;
+        }
+        for(int j = 1; j<=i;j++){
+            cout<<start %2;
+            start++;
+           
+            
+        }
+         cout<<endl;
+            
+            }
+            
+    }
